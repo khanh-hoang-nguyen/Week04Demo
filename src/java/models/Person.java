3bcf97dd7ffe1c2aa63d0da4c240f2e03c149e12
pandawr <pandawr@10.211.55.3>
2022-09-25 14:06:59 -0600

    Add person bean

---
+++ src/java/models/Person.java
@@ -0,0 +1,43 @@
+
+package models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author pandawr
+ */
+public class Person implements Serializable {
+    private String firstname;
+    private String lastname;
+
+    public Person() {
+        firstname = "";
+        firstname = "";
+    }
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+    
+    
+}
