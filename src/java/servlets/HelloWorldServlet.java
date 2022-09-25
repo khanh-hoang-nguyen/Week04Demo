4a6796fcdff0d80fff0873bb62812466039d455c
pandawr <pandawr@10.211.55.3>
2022-09-25 14:17:43 -0600

    Converted application into a full MVC pattern

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -1,4 +1,3 @@
-
 package servlets;
 
 import java.io.IOException;
@@ -7,12 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
  * @author pandawr
  */
 public class HelloWorldServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -27,22 +28,24 @@ public class HelloWorldServlet extends HttpServlet {
         // get the two parameters from the POST request
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
-        
-        // set the attributes for the JSP
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
-        
+
         // validation: if the parameters don't exist or are empty, show the first page again
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
-            // set an attribute for a message
+        // set the attributes for the JSP
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
+        // set an attribute for a message
             request.setAttribute("message", "Invalid entry. Please enter both your first and last names.");
-            
+
             // forward the request and response objects to the JSP
             // display the form again
             getServletContext().getRequestDispatcher("/WEB-INF/hello.jsp")
                     .forward(request, response);
             return; // very important!
         }
+
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
 
         // display the second page
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
